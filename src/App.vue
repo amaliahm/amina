ac575e396916bde3264369436893652c80443094
amaliahm <a.ghandouz@esi-sba.dz>
2024-08-20 17:05:50 +0100

    disable inspect/right click

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,67 @@ import AboutMe from "./_about-me/About.vue";
 import Projects from "./_projects/Projects.vue";
 import Contact from "./_contact-me/Contact.vue";
 
-
-
 const navStore = useNavStore();
+const showWarning = ref(false);
+const msgWarning = ref('');
+
+const hideWarning = () => {
+  showWarning.value = false;
+  msgWarning.value = '';
+};
+
+onMounted(() => {
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    showWarning.value = true;
+    msgWarning.value = 'Right-click is disabled';
+    setTimeout(hideWarning, 3000); 
+  });
+
+  document.addEventListener("keydown", (e) => {
+    // disable f12
+    if ( e.keyCode === 123 ) {
+      e.preventDefault();
+      showWarning.value = true;
+      msgWarning.value = 'F12 key is disabled';
+      setTimeout(hideWarning, 3000);
+    }
+    // disable ctrl+shift+i
+    if ( e.ctrlKey && e.shiftKey && e.keyCode === 73 ) {
+      e.preventDefault();
+      showWarning.value = true;
+      msgWarning.value = 'Inspect is disabled';
+      setTimeout(hideWarning, 3000);
+    }
+    // disable ctrl+shift+j
+    if ( e.ctrlKey && e.shiftKey && e.keyCode === 74 ) {
+      e.preventDefault();
+      showWarning.value = true;
+      msgWarning.value = 'Inspect is disabled';
+      setTimeout(hideWarning, 3000);
+    }
+    // disable ctrl+u
+    if ( e.ctrlKey && e.keyCode === 85 ) {
+      e.preventDefault();
+      showWarning.value = true;
+      msgWarning.value = 'Ctrl+U is disabled';
+      setTimeout(hideWarning, 3000);
+    }
+  });
+});
 </script>
 
 <template>
   <div
-    class="min-h-screen h-screen lg:max-h-screen lg:max-w-screen w-full flex flex-col overflow-hidden"
+    class="min-h-screen h-screen lg:max-h-screen lg:max-w-screen w-full flex flex-col overflow-hidden relative"
   >
     <NavigationBar />
     <NavigationBarMobile />
     <Transition 
       name="fade" 
       mode="out-in" 
-      appear @after-leave="onAfterLeave"
+      appear 
+      @after-leave="onAfterLeave"
     >
       <template 
         v-if="navStore.activeElement == '_hello'"
@@ -43,8 +89,13 @@ const navStore = useNavStore();
         <Contact />
       </template>
     </Transition>
-
     <FooterBar />
+    <div
+      v-if="showWarning"
+      class="fixed bottom-5 right-5 bg-gradient-to-br from-[#4D5BCE] to-[#43D9AD] text-white p-4 rounded-lg shadow-lg z-50 transition-all duration-500 ease-in-out"
+    >
+      <p> {{msgWarning}} </p>
+    </div>
   </div>
 </template>
 
